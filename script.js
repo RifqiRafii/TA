d762aecdfc612aca1ab1c548557859bd670685c3
RifqiRafii <rifqirafi28@gmail.com>
2025-06-10 23:48:19 +0700

    Dashboard - Change Color Theme

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+    let today = new Date()
+    let formattedDate = today.toLocaleDateString('id-ID', {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    })
+
+    $('#date').text(formattedDate)
+
     $('#updateProfile').click(function(){
         if ((!($("#change-email").val()).includes("@")) || (($("#change-name").val()).trim() == "")) {
             alert("Username atau Email tidak Valid")
